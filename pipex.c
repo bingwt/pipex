6adffea5ea6ac9132844367e844448240909a032
Bing Wen Tan <btan@student.42singapore.sg>
2023-12-15 02:42:04 +0800

    modified:   pipex.c

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: btan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 01:00:24 by btan              #+#    #+#             */
-/*   Updated: 2023/12/15 01:59:06 by btan             ###   ########.fr       */
+/*   Updated: 2023/12/15 02:38:46 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,51 @@ static void	run_cmd(char *args)
 	free(program);
 }
 
+static void	redirect(int fd, int *p_fd, int dir)
+{
+	if (dir == 0)
+		dup2(p_fd[1], 1);
+	else if (dir == 1)
+		dup2(p_fd[0], 0);
+	dup2(fd, dir);
+	close(fd);
+	close(p_fd[0]);
+	close(p_fd[1]);
+}
+
+int	main(int argc, char **argv)
+{
+	int	p_fd[2];
+	int	pid1;
+	int	pid2;
+	int	infile;
+	int	outfile;
+
+	infile = open(argv[1], O_RDONLY);
+	outfile = open(argv[4], O_WRONLY | O_CREAT, 0644);
+	pipe(p_fd);
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		dup2(infile, 0);
+		close(infile);
+		dup2(p_fd[1], 1);
+		close(p_fd[0]);
+		close(p_fd[1]);
+		run_cmd(argv[2]);
+	}
+	pid2 = fork();
+	if (pid2 == 0)
+	{
+		dup2(p_fd[0], 0);
+		close(p_fd[0]);
+		close(p_fd[1]);
+		dup2(outfile, 1);
+		close(outfile);
+		run_cmd(argv[3]);
+	}
+	close(p_fd[0]);
+	close(p_fd[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+}
