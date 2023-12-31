9071c5b274159a39d5b0bd0ff24055938c1de2fa
Bing Wen Tan <btan@student.42singapore.sg>
2024-01-01 00:45:43 +0800

    modified:   Makefile 	modified:   pipex.c 	modified:   pipex.h

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: btan <btan@student.42singapore.sg>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/11/06 15:22:23 by btan              #+#    #+#              #
-#    Updated: 2023/12/15 17:26:52 by btan             ###   ########.fr        #
+#    Updated: 2023/12/19 14:30:11 by btan             ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -38,14 +38,23 @@ fclean: clean
 
 re: fclean all
 
+test-pipex: re
+	./pipex tests/infile cat ls tests/outfile
+	cat tests/outfile
+	rm tests/outfile
+
 test-make:
 	make -C Libft
 	mv ./Libft/libft.a ./
 	make fclean -C Libft
+
 test: test-make
 	$(CC) tests/main.c -o example -I. -ILibft libft.a -g
+
 run:
 	clear && ./a.out
+
 tester:
 	$(CC) tests/test_pipe.c -L -l $(NAME) -o tester && ./tester && rm tester
+
 .PHONY: all clean fclean re
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: btan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 01:00:24 by btan              #+#    #+#             */
-/*   Updated: 2023/12/15 18:40:50 by btan             ###   ########.fr       */
+/*   Updated: 2023/12/19 17:23:54 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,23 +32,17 @@ static void	run_cmd(char *args)
 		ft_putstr_fd(": Command not found\n", 2);
 		exit(127);
 	}
+	waitpid(pid, NULL, 0);
 	ptr = cmd;
 	while (*cmd)
 		free(*(cmd++));
 	free(ptr);
 	free(program);
+	exit(0);
 }
 
-static void	rd_cmd(char *file, int *p_fd, int dir)
+static void	rd_cmd(int fd, int *p_fd, int dir)
 {
-	int	fd;
-
-	if (dir == 1)
-		fd = open(file, O_RDONLY);
-	else
-		fd = open(file, O_WRONLY | O_CREAT, 0644);
-	if (fd == -1)
-			perror(NULL);
 	dup2(fd, !dir);
 	close(fd);
 	dup2(p_fd[dir], dir);
@@ -60,6 +54,7 @@ void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile)
 {
 	int	p_fd[2];
 	int	pid;
+	int	fd;
 
 	pipe(p_fd);
 	pid = fork();
@@ -67,13 +62,28 @@ void	pipex(char *infile, char *cmd1, char *cmd2, char *outfile)
 			perror(NULL);
 	if (pid == 0)
 	{
-		rd_cmd(infile, p_fd, 1);
+		fd = open(infile, O_RDONLY);
+		if (fd == -1)
+			exit(1);
+		rd_cmd(fd, p_fd, 1);
 		run_cmd(cmd1);
+		close(fd);
+		exit(0);
 	}
 	waitpid(pid, NULL, 0);
-	rd_cmd(outfile, p_fd, 0);
-	run_cmd(cmd2);
-	//ft_printf("%s\n", outfile);
-	//ft_printf("%s\n", cmd2);
-	exit(127);
+	pid = fork();
+	if (pid == -1)
+			perror(NULL);
+	if (pid == 0)
+	{
+		fd = open(outfile, O_WRONLY | O_CREAT, 0644);
+		if (fd == -1)
+			exit(1);
+		rd_cmd(fd, p_fd, 0);
+		run_cmd(cmd2);
+		close(fd);
+		exit(0);
+	}
+	waitpid(pid, NULL, 0);
+	exit(0);
 }
