6f66782a13c4d15dbea785c08e12fe142bcc3224
Bing Wen Tan <btan@c2r5s8.42singapore.sg>
2024-01-10 15:37:10 +0800

    modified:   main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: btan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 03:05:11 by btan              #+#    #+#             */
-/*   Updated: 2024/01/10 15:34:25 by btan             ###   ########.fr       */
+/*   Updated: 2024/01/10 15:37:07 by btan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-//	char	*path;
-
-//	path = get_path(envp, "catd");
-//	ft_printf("main: %s\n", path);
-//	if (path)
-//		free(path);
 	if (argc != 5)
 	{
 		ft_printf("Please provide 4 arguments!\n");
